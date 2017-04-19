0d56dce8d9e4c0b6dfdf39fbe8d4e4d1597c0ac9
Mark Wooff <mark.wooff@gmail.com>
2017-04-19 16:20:47 -0400

    API for Event Calendar

--- mod/gc_mobile_api/inc/functions.php
+++ mod/gc_mobile_api/inc/functions.php
@@ -64,3 +64,7 @@ function wire_filter( $text ){
 
 	return $text;
 }
+
+function clean_text( $text ){
+	return trim( preg_replace('/ +/', ' ', preg_replace('/[^A-Za-z0-9 ]/', ' ', urldecode( html_entity_decode( strip_tags( $text ) ) ) ) ) );
+}
