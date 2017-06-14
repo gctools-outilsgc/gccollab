dab03bd022becd954026995b9f132e2e257bbaa6
Mark Wooff <mark.wooff@gmail.com>
2017-06-14 11:21:24 -0400

    Merge `missions_profile_extend` mod into `b_extended_profile`

--- mod/b_extended_profile/languages/fr.php
+++ mod/b_extended_profile/languages/fr.php
@@ -19,18 +19,6 @@ $french = array(
     'profile:title' => "%s's Profile", /* NEW */
     'profile:contactinfo' => 'Contact Info', /* NEW */
 
-    'gcconnex_profile:basic:user_type' => "Occupation",
-    'gcconnex_profile:basic:institution' => "Institution",
-    'gcconnex_profile:basic:current_occupation' => "Current Occupation as %s at %s (Translate please)",
-    
-    'gcconnex-profile-card:student' => 'Student',
-    'gcconnex-profile-card:academic' => 'Academic',
-    'gcconnex-profile-card:public_servant' => 'Public Servant',
-
-    "gcconnex-profile-card:uottawa" => "University of Ottawa",
-    "gcconnex-profile-card:uwaterloo" => "University of Waterloo",
-    "gcconnex-profile-card:ucarleton" => "Carleton University",
-
     //user Settings Details
     'item:object:MySkill' => 'Compétences inscrites dans le profil',
     'item:object:experience' => 'Expérience de travail inscrite dans le profil',
@@ -175,7 +163,61 @@ $french = array(
     'gcconnex_profile:portfolio:publication_date' => 'Date de publication ',
     'gcconnex_profile:portfolio:datestamp' => 'Aucune date de publication',
     'gcconnex_profile:portfolio:description' => 'Description ',
-    'gcconnex_profile:portfolio:delete' => 'Supprimer cette entrée'
+    'gcconnex_profile:portfolio:delete' => 'Supprimer cette entrée',
+
+/////////////// Opportunities
+        // labels
+        "gcconnex_profile:opt:opt-in" => "S'inscrire",
+        "gcconnex_profile:opt:set_empty" => "Aucune option ont été sélectionnés.",
+        "gcconnex_profile:opt:micro_mission" => "Micro-Mission",
+        "gcconnex_profile:opt:job_swap" => "Échange d'emploi",
+        "gcconnex_profile:opt:mentored" => "Mentoré",
+        "gcconnex_profile:opt:mentoring" => "Mentor",
+        "gcconnex_profile:opt:shadowed" => "Observé",
+        "gcconnex_profile:opt:shadowing" => "Observateur",
+        "gcconnex_profile:opt:peer_coached" => "Pair entraîné",
+        "gcconnex_profile:opt:peer_coaching" => "Pair entraineur",
+        "gcconnex_profile:opt:opt_in_access" => "Qui peut voir mes choix d'inscription :",
+        "gcconnex_profile:opt:yes"=> "Oui",
+        "gcconnex_profile:opt:no" => "Non",
+        "gcconnex_profile:completed_missions" => "Possibilités complètés grâce à la plateforme de ConneXions carrière",
+        "gcconnex_profile:opt:skill_sharing" => "Partage des habiletés",
+        "gcconnex_profile:opt:job_sharing" => "Partage d'emploi",
+
+        // opportunity types opt-in
+            // at-level
+    "gcconnex_profile:opt:atlevel"=>"Mobilité au même niveau",
+        "gcconnex_profile:opt:assignment_deployment_seek" => "Détachements / Affectations participant",
+        "gcconnex_profile:opt:assignment_deployment_create" => "Détachements / Affectations hôte",
+        "gcconnex_profile:opt:deployment_seek" => "Mutation participant",
+        "gcconnex_profile:opt:deployment_create" => "Mutation hôte",
+        "gcconnex_profile:opt:job_swap" => "Échange d'emploi",
+        "gcconnex_profile:opt:job_rotate" => "Rotation d'emploi",
+
+            // Developmental
+        "gcconnex_profile:opt:micro_missionseek" => "Micro-Mission participant",
+        "gcconnex_profile:opt:micro_mission" => "Micro-Mission hôte",
+        "gcconnex_profile:opt:shadowing" => "Observation au poste de travail participant",
+        "gcconnex_profile:opt:shadowed" => "Observation au poste de travail hôte",
+        "gcconnex_profile:opt:mentored" => "Mentoré",
+        "gcconnex_profile:opt:mentoring" => "Mentor",
+        "gcconnex_profile:opt:job_sharing" => "Partage d'emploi",
+        "gcconnex_profile:opt:peer_coached" => "Pair entraîné",
+        "gcconnex_profile:opt:peer_coaching" => "Pair entraineur",
+        "gcconnex_profile:opt:skill_sharing" => "Partage des habiletés participant",
+        "gcconnex_profile:opt:skill_sharing_create" => "Partage des habiletés hôte",
+            "gcconnex_profile:opt:participants"=>"Participant",
+        "gcconnex_profile:opt:host"=>"Hôte",
+    
+    "gcconnex_profile:opt:development"=>"Possibilités de perfectionnement professionnel",
+        
+        // SEPARATION
+        
+        "gcconnex_profile:opportunities" => "Opportunités",
+        "gcconnex_profile:hide" => "Cacher",
+        "gcconnex_profile:confirm:hide" => "Cacher ce Micro-Mission signifie qu'il ne sera plus visible sous 'Possibilités complètés grâce à la plateforme de ConneXions carrière' dans votre profil et cette opération ne peut pas être inversée.",
+        "gcconnex_profile:show" => "Montrer"
+
 );
 
 add_translation("fr",$french);
