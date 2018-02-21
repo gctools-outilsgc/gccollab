1b731c26d2fc556567b59106b61dc6a80ad8e98b
BrandonGould <34379222+bmrgould@users.noreply.github.com>
2018-02-21 14:38:33 -0500

    get bookmarks by user

--- mod/gc_mobile_api/models/bookmark.php
+++ mod/gc_mobile_api/models/bookmark.php
@@ -35,6 +35,23 @@ elgg_ws_expose_function(
 	false
 );
 
+elgg_ws_expose_function(
+  "get.bookmarksbyuser",
+  "get_bookmarks_by_user",
+  array(
+    "user" => array('type' => 'string', 'required' => true),
+    "limit" => array('type' => 'int', 'required' => false, 'default' => 10),
+    "offset" => array('type' => 'int', 'required' => false, 'default' => 0),
+    "filters" => array('type' => 'string', 'required' => false, 'default' => ""),
+    "lang" => array('type' => 'string', 'required' => false, 'default' => "en"),
+    "target" => array('type' => 'string', 'required' => false, 'default' => "")
+  ),
+  'Retrieves bookmarks posed by a user',
+  'POST',
+  true,
+  false
+);
+
 function get_bookmarks($user, $limit, $offset, $filters, $lang)
 {
     // Check provided USER information.
@@ -145,4 +162,59 @@ function get_bookmarks_colleague($user, $limit, $offset, $filters, $lang)
 
   return $bookmarks;
 
+}
+
+function get_bookmarks_by_user($user, $limit, $offset, $filters, $lang, $target)
+{
+  // Check provided USER information.
+  $user_entity = is_numeric($user) ? get_user($user) : (strpos($user, '@') !== false ? get_user_by_email($user)[0] : get_user_by_username($user));
+  if (!$user_entity) {
+    return "User was not found. Please try a different GUID, username, or email address";
+  }
+  if (!$user_entity instanceof ElggUser) {
+    return "Invalid user. Please try a different GUID, username, or email address";
+  }
+  if (!elgg_is_logged_in()) {
+    login($user_entity);
+  }
+
+  //add conditional for target later
+  $all_bookmarks = elgg_list_entities(array(
+    'type' => 'object',
+    'subtype' => 'bookmarks',
+    'owner_guid' => $user,
+    'limit' => $limit,
+    'offset' => $offset
+  ));
+  $bookmarks = json_decode($all_bookmarks);
+  
+  foreach ($bookmarks as $bookmark) {
+    $bookmark->title = gc_explode_translation($bookmark->title, $lang);
+
+    $likes = elgg_get_annotations(array(
+			'guid' => $bookmark->guid,
+			'annotation_name' => 'likes'
+		));
+		$bookmark->likes = count($likes);
+		$liked = elgg_get_annotations(array(
+			'guid' => $bookmark->guid,
+			'annotation_owner_guid' => $user_entity->guid,
+			'annotation_name' => 'likes'
+        ));
+    $bookmark->liked = count($liked) > 0;
+
+    $bookmarkObject = get_entity($bookmark->guid);
+    $bookmark->description = gc_explode_translation($bookmarkObject->description, $lang);
+    $bookmark->address = $bookmarkObject->address;
+
+    $bookmark->userDetails = get_user_block($bookmark->owner_guid, $lang); //Should go through and only pass revelant infromation
+    $bookmark->group_guid = "";
+    if ($bookmark->container_guid != $bookmark->owner_guid){
+      $bookmark->group_guid = $bookmark->container_guid;
+      $bookmarkGroup = get_entity($bookmark->group_guid);
+      $bookmark->group = gc_explode_translation($bookmarkGroup->name, $lang);
+    }
+  }
+
+  return $bookmarks;
 }
