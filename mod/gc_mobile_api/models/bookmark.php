ff6104a7a11ec5ccd56b256238dc006b3fb05501
BrandonGould <34379222+bmrgould@users.noreply.github.com>
2018-02-22 12:15:05 -0500

    Bookmarks API - get Bookmark

--- mod/gc_mobile_api/models/bookmark.php
+++ mod/gc_mobile_api/models/bookmark.php
@@ -2,6 +2,19 @@
 /*
 * Exposes API endpoints for Bookmark entities
 */
+elgg_ws_expose_function(
+	"get.bookmark",
+	"get_bookmark",
+	array(
+		"user" => array('type' => 'string', 'required' => true),
+		"guid" => array('type' => 'int', 'required' => true),
+		"lang" => array('type' => 'string', 'required' => false, 'default' => "en")
+	),
+	'Retrieves an bookmark based on user id and bookmark id',
+	'POST',
+	true,
+	false
+);
 
 elgg_ws_expose_function(
     "get.bookmarks",
@@ -51,6 +64,63 @@ elgg_ws_expose_function(
   true,
   false
 );
+
+function get_bookmark($user, $guid, $lang)
+{
+  // Check provided USER information.
+  $user_entity = is_numeric($user) ? get_user($user) : (strpos($user, '@') !== false ? get_user_by_email($user)[0] : get_user_by_username($user));
+  if (!$user_entity) {
+    return "User was not found. Please try a different GUID, username, or email address";
+  }
+  if (!$user_entity instanceof ElggUser) {
+    return "Invalid user. Please try a different GUID, username, or email address";
+  }
+  if (!elgg_is_logged_in()) {
+    login($user_entity);
+  }
+
+  $entity = get_entity($guid);
+  if (!$entity) {
+    return "Event was not found. Please try a different GUID";
+  }
+  //check if entity is bookmark? subtype bookmarks
+
+  $bookmark_ent = elgg_list_entities(array(
+    'type' => 'object',
+    'subtype' => 'bookmarks',
+    'guid' => $guid
+  ));
+  $bookmarks = json_decode($bookmark_ent);
+
+  foreach ($bookmarks as $bookmark) {
+    $bookmark->title = gc_explode_translation($bookmark->title, $lang);
+
+    $likes = elgg_get_annotations(array(
+			'guid' => $bookmark->guid,
+			'annotation_name' => 'likes'
+		));
+		$bookmark->likes = count($likes);
+		$liked = elgg_get_annotations(array(
+			'guid' => $bookmark->guid,
+			'annotation_owner_guid' => $user_entity->guid,
+			'annotation_name' => 'likes'
+        ));
+    $bookmark->liked = count($liked) > 0;
+
+    $bookmarkObject = get_entity($bookmark->guid);
+    $bookmark->description = gc_explode_translation($bookmarkObject->description, $lang);
+    $bookmark->address = $bookmarkObject->address;
+
+    $bookmark->userDetails = get_user_block($bookmark->owner_guid, $lang); //Should go through and only pass revelant infromation
+    $bookmark->group_guid = "";
+    if ($bookmark->container_guid != $bookmark->owner_guid){
+      $bookmark->group_guid = $bookmark->container_guid;
+      $bookmarkGroup = get_entity($bookmark->group_guid);
+      $bookmark->group = gc_explode_translation($bookmarkGroup->name, $lang);
+    }
+  }
+  return $bookmarks;
+}//end get_bookmark
 
 function get_bookmarks($user, $limit, $offset, $filters, $lang)
 {
