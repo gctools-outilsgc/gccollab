d51a1ccf4e28b5e1490d1cbb9ad6c6a87971a72e
BrandonGould <34379222+bmrgould@users.noreply.github.com>
2018-03-20 09:50:22 -0400

    Change Test To Wire

--- mod/gc_mobile_api/models/test.php
+++ mod/gc_mobile_api/models/test.php
@@ -2,15 +2,15 @@
 
 
 elgg_ws_expose_function(
-	"submit.commenttest",
-	"submit_comment_test",
+	"get.wireposttest",
+	"get_wirepost_test",
 	array(
 		"user" => array('type' => 'string', 'required' => true),
 		"guid" => array('type' => 'int', 'required' => true),
-		"message" => array('type' => 'string', 'required' => true),
+		"thread" => array('type' => 'int', 'required' => false, 'default' => 0),
 		"lang" => array('type' => 'string', 'required' => false, 'default' => "en")
 	),
-	'Gets comments for given entity (guid), uses limit and offset.',
+	'Retrieves a wire post & all replies based on user id and wire post id',
 	'POST',
 	true,
 	false
@@ -18,13 +18,8 @@ elgg_ws_expose_function(
 
 
 
-function submit_comment_test($user, $guid, $message, $lang)
+function get_wirepost_test($user, $guid, $thread, $lang)
 {
-	// No Empty Comments
-	if (empty($message)){
-		return elgg_echo("generic_comment:blank");
-	}
-
 	$user_entity = is_numeric($user) ? get_user($user) : (strpos($user, '@') !== false ? get_user_by_email($user)[0] : get_user_by_username($user));
 	if (!$user_entity) {
 		return "User was not found. Please try a different GUID, username, or email address";
@@ -32,72 +27,137 @@ function submit_comment_test($user, $guid, $message, $lang)
 	if (!$user_entity instanceof ElggUser) {
 		return "Invalid user. Please try a different GUID, username, or email address";
 	}
+
+	$entity = get_entity($guid);
+	if (!$entity) {
+		return "Wire was not found. Please try a different GUID";
+	}
+	if (!$entity instanceof ElggWire) {
+		return "Invalid wire. Please try a different GUID";
+	}
+
+	$thread_id = $entity->wire_thread;
+
 	if (!elgg_is_logged_in()) {
 		login($user_entity);
 	}
 
-	$entity = get_entity($guid);
-	$subtype = 'nothing';
-	if (!$entity) {
-		return elgg_echo("generic_comment:notfound");
-	} else if (elgg_instanceof($entity, "object", "groupforumtopic")){
-		$subtype = 'discussion_reply';
-		// Check if member of group
-		$group = $entity->getContainerEntity();
-		if (!$group->canWriteToContainer()) {
-			return elgg_echo('groups:notmember');
+	if ($thread) {
+		$all_wire_posts = elgg_list_entities_from_metadata(array(
+			"metadata_name" => "wire_thread",
+			"metadata_value" => $thread_id,
+			"type" => "object",
+			"subtype" => "thewire",
+			"limit" => 0,
+			"preload_owners" => true
+		));
+		$wire_posts = json_decode($all_wire_posts);
+
+		foreach ($wire_posts as $wire_post) {
+			$wire_post_obj = get_entity($wire_post->guid);
+			$reshare = $wire_post_obj->getEntitiesFromRelationship(array("relationship" => "reshare", "limit" => 1))[0];
+
+			$url = "";
+			if (!empty($reshare)) {
+				$url = $reshare->getURL();
+			}
+
+			$text = "";
+			if (!empty($reshare->title)) {
+				$text = $reshare->title;
+			} elseif (!empty($reshare->name)) {
+				$text = $reshare->name;
+			} elseif (!empty($reshare->description)) {
+				$text = elgg_get_excerpt($reshare->description, 140);
+			}
+
+			$wire_post->shareURL = $url;
+			$wire_post->shareText = gc_explode_translation($text, $lang);
+
+			$likes = elgg_get_annotations(array(
+				'guid' => $wire_post->guid,
+				'annotation_name' => 'likes'
+			));
+			$wire_post->likes = count($likes);
+
+			$liked = elgg_get_annotations(array(
+				'guid' => $wire_post->guid,
+				'annotation_owner_guid' => $user_entity->guid,
+				'annotation_name' => 'likes'
+			));
+			$wire_post->liked = count($liked) > 0;
+
+			$replied = elgg_get_entities_from_metadata(array(
+				"metadata_name" => "wire_thread",
+				"metadata_value" => $thread_id,
+				"type" => "object",
+				"subtype" => "thewire",
+				"owner_guid" => $user_entity->guid
+			));
+			$wire_post->replied = count($replied) > 0;
+
+			$wire_post->thread_id = $thread_id;
+
+			$wire_post->userDetails = get_user_block($wire_post->owner_guid, $lang);
+			$wire_post->description = wire_filter($wire_post->description);
 		}
 	} else {
-		$subtype = 'comment';
-	}
+		$wire_posts = elgg_list_entities(array(
+			"type" => "object",
+			"subtype" => "thewire",
+			"guid" => $guid
+		));
+		$wire_post = json_decode($wire_posts)[0];
 
+		$wire_post_obj = get_entity($wire_post->guid);
+		$reshare = $wire_post_obj->getEntitiesFromRelationship(array("relationship" => "reshare", "limit" => 1))[0];
 
-	$reply = '';
-	if ($subtype == 'discussion_reply'){
-		$reply = new ElggDiscussionReply();
-	} else if ($subtype == 'comment'){
-		$reply = new ElggComment();
-	}
-
-	$reply->description = $message;
-	$reply->owner_guid = $user_entity->getGUID();
-	$reply->container_guid =  $entity->getGUID();
-	$reply->access_id = $entity->access_id; //idk
-
-	$reply_guid = $reply->save();
-	// If save comment fails, return message about it
-	if ($reply_guid == false){
-		if ($subtype == 'discussion_reply') {
-			return elgg_echo('groupspost:failure');
-		} else if ($subtype == 'comment'){
-			return elgg_echo("generic_comment:failure");
-		} else {
-			return "reply failed - unknown - ref 00001 "; //this shouldnt happen
+		$url = "";
+		if (!empty($reshare)) {
+			$url = $reshare->getURL();
 		}
+
+		$text = "";
+		if (!empty($reshare->title)) {
+			$text = $reshare->title;
+		} elseif (!empty($reshare->name)) {
+			$text = $reshare->name;
+		} elseif (!empty($reshare->description)) {
+			$text = elgg_get_excerpt($reshare->description, 140);
+		}
+
+		$wire_post->shareURL = $url;
+		$wire_post->shareText = gc_explode_translation($text, $lang);
+
+		$likes = elgg_get_annotations(array(
+			'guid' => $wire_post->guid,
+			'annotation_name' => 'likes'
+		));
+		$wire_post->likes = count($likes);
+
+		$liked = elgg_get_annotations(array(
+			'guid' => $wire_post->guid,
+			'annotation_owner_guid' => $user_entity->guid,
+			'annotation_name' => 'likes'
+		));
+		$wire_post->liked = count($liked) > 0;
+
+		$replied = elgg_get_entities_from_metadata(array(
+			"metadata_name" => "wire_thread",
+			"metadata_value" => $thread_id,
+			"type" => "object",
+			"subtype" => "thewire",
+			"owner_guid" => $user_entity->guid
+		));
+		$wire_post->replied = count($replied) > 0;
+
+		$wire_post->thread_id = $thread_id;
+
+		$wire_post->userDetails = get_user_block($wire_post->owner_guid, $lang);
+		$wire_post->description = wire_filter($wire_post->description);
+
+		$wire_posts = $wire_post;
 	}
 
-
-	//River Item
-	if ($subtype == 'discussion_reply') {
-		elgg_create_river_item(array(
-		'view' => 'river/object/discussion_reply/create',
-		'action_type' => 'reply',
-		'subject_guid' => $user_entity->guid,
-		'object_guid' => $reply_guid,
-		'target_guid' => $entity->guid,
-		));
-		return elgg_echo('groupspost:success');
-	} else if ($subtype == 'comment'){
-		elgg_create_river_item(array(
-			'view' => 'river/object/comment/create',
-			'action_type' => 'comment',
-			'subject_guid' => $user_entity->guid,
-			'object_guid' => $reply_guid,
-			'target_guid' => $entity->guid,
-		));
-		return elgg_echo('generic_comment:posted');
-	}
-
-	$result = "Should not reach this, but in-case, ref 00002";
-	return $result;
+	return $wire_posts;
 }
